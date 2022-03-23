01928a064aef78bc19f845eaaaa3645cca2ed00b
Naranbaatar <b18fa1416@ufe.edu.mn>
2022-03-23 10:08:52 +0800

    Navbar and footer

---
+++ components/Footer.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+      <div className="bg-blue-500 h1-1/2 w-full flex md:flex-row flex-col justify-around items-start p-20">
+    <ul>
+      <p className="text-gray-50 font-bold text-xl pb-5">
+        Zulguur.mn
+      </p>
+      <div className="text-gray-50 flex text:xs">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br>
+      Ut nulla eros, luctus eu accumsan eget, eleifend placerat dui.
+      </div>
+    </ul>
+    <div className="p-5">
+    <ul>
+      <p className="text-gray-50 font-bold text-md pb-4">Zulguur.mn</p>
+      <li className="text-gray-50 text-xs pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+        Бидний тухай
+      </li>
+      <li className="text-gray-50 text-xs pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+        Үйлчилгээ
+      </li>
+      <li className="text-gray-50 text-xs pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+        Түгээмэл асуултууд
+      </li>
+    </ul>
+  </div>
+  <div className="p-5">
+    <ul>
+      <p className="text-gray-50 font-bold text-md pb-4">Холбоо барих</p>
+      <li className="text-gray-50 text-xs pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+        Утас: 99999999
+      </li>      
+      <li className="text-gray-50 text-xs pb-2 font-semibold hover:text-blue-600 cursor-pointer">
+        Имэйл: info@Zulguur.mn
+        </li> 
+    </ul>
+  </div>
+  </div>
+      </footer>
+    </>
+  );
+}
---
+++ components/Navbar.js
@@ -0,0 +1,76 @@
+import React from "react";
+import Link from "next/link";
+
+
+
+export default function Navbar(props) {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  return (
+    <>
+      <nav className="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <Link href="/">
+              <a
+                className="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+                href="#pablo"
+              >
+                Zulguur.mn
+              </a>
+            </Link>
+          </div>
+          <div
+           
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="flex items-center">
+                <a
+                  className="hover:text-blueGray-500 text-blueGray-700  flex items-center text-xs uppercase font-bold"
+                  href="/"
+                >
+                  <i className="cursor-pointer text-md hover:font-black" />
+                  Нүүр
+                </a>
+              </li>
+              <li className="flex items-center">
+                <a
+                  className="hover:text-blueGray-500 text-blueGray-700  flex items-center text-xs uppercase font-bold"
+                  href="#CarWash"
+                >
+                  <i className="cursor-pointer font-semibold px-3 py-2 text-md hover:font-black" />
+                  Угаалгын газрууд
+                </a>
+              </li><li className="flex items-center">
+                <a
+                  className="hover:text-blueGray-500 text-blueGray-700  flex items-center text-xs uppercase font-bold"
+                  href="#Order"
+                >
+                  <i className="cursor-pointer font-semibold px-3 py-2 text-md hover:font-black" />
+                  Цаг захиалах
+                </a>
+              </li><li className="flex items-center">
+                <a
+                  className="hover:text-blueGray-500 text-blueGray-700 flex items-center text-xs uppercase font-bold"
+                  href="#about"
+                >
+                  <i className="cursor-pointer font-semibold px-3 py-2 text-md hover:font-black" />
+                  Бидний тухай
+                </a>
+              </li>
+
+              {/* Profile Icon oruulna */}
+              {/* <li className="flex items-center">
+                <a
+                  className="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
+                  href="#Profile"
+                >
+                </a>
+              </li> */}
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
